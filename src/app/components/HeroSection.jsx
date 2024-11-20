2dada26c48a6c7eeeb8cc97ab8da81bbd643b5b3
Salah Mohamed <salahmohamedalzeini@gmail.com>
2024-11-20 18:14:55 +0200

    updte eslin1

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -49,7 +49,7 @@ const HeroSection = () => (
         <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
           <Image
             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 "
-            src="/_next/static/media/hero-image.3372c4ff.png"
+            src="/images/hero-image.png"
             alt="hero image"
             width={300}
             height={300}
